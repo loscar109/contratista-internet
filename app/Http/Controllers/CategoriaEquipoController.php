b34c5bc1b12b7cad6fbe182b5ee90ceb5da59a41
loscar109 <charly_groso_64@hotmail.com>
2023-12-19 10:39:43 -0300

    creando equipos y categorias

---
+++ app/Http/Controllers/CategoriaEquipoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoriaEquipoRequest;
+use App\Http\Requests\UpdateCategoriaEquipoRequest;
+use App\Models\CategoriaEquipo;
+
+class CategoriaEquipoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoriaEquipoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoriaEquipo $categoriaEquipo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CategoriaEquipo $categoriaEquipo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoriaEquipoRequest $request, CategoriaEquipo $categoriaEquipo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoriaEquipo $categoriaEquipo)
+    {
+        //
+    }
+}
