564eeec0d03d13bf06da04157f7c6d64e5ea6748
loscar109 <charly_groso_64@hotmail.com>
2023-12-21 08:18:53 -0300

    seeder de Equipo y Categorias

--- database/seeders/CategoriaEquipoSeeder.php
+++ database/seeders/CategoriaEquipoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\CategoriaEquipo;
 
 class CategoriaEquipoSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class CategoriaEquipoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        CategoriaEquipo::create(['nombre' => 'HD','descripcion' => 'Equipo que sirve para deodificar canales a partir de 720p']);
+        CategoriaEquipo::create(['nombre' => 'WIFI','descripcion' => 'Equipo para conectarse a internet']);
+        CategoriaEquipo::create(['nombre' => 'CM','descripcion' => 'CableModem para sistemas de conexión a internet antiguos']);
+        CategoriaEquipo::create(['nombre' => 'DCT','descripcion' => 'Equipo antiguo para decodificar canales a 480p']);
+        CategoriaEquipo::create(['nombre' => 'ISDBT','descripcion' => 'Equipo destinado a la transmisión de radio digital y televisión digital']);
+
     }
 }
--- database/seeders/EquipoSeeder.php
+++ database/seeders/EquipoSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Equipo;
+
 
 class EquipoSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class EquipoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Equipo::create(['mac' => 'PAER0073476','categoria_equipos_id' => 1]);
+        Equipo::create(['mac' => 'HP44H23200193682','categoria_equipos_id' => 1]);
+        Equipo::create(['mac' => 'ALCLB412744A','categoria_equipos_id' => 2]);
+        Equipo::create(['mac' => 'M90738FG4102','categoria_equipos_id' => 4]);
+        Equipo::create(['mac' => 'CDV220533623','categoria_equipos_id' => 5]);
+
+
+
     }
 }
