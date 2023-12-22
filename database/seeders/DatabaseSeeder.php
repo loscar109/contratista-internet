109218da0abd831228ad04c3321b06af9ed2fb68
loscar109 <charly_groso_64@hotmail.com>
2023-12-22 06:31:08 -0300

    user completo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(UserSeeder::class);
         $this->call(CategoriaEquipoSeeder::class);
         $this->call(EmpleadoSeeder::class);
         $this->call(EquipoSeeder::class);
