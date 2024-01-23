9a9b51ee91542005cc2cf038eb989df81583fa37
loscar109 <charly_groso_64@hotmail.com>
2024-01-23 08:08:29 -0300

    integrando adminlte de manera asertiva

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+@section('titulo')
+    <div class="col-sm-6">
+        <h1 class="m-0">Home</h1>
     </div>
-</div>
 @endsection
+
+
+@section('navegacion')
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="home">Home</a></li>
+        </ol>
+    </div>
+@endsection
+
+@section('contenido')
+    <p>Soy parte del home</p>
+@endsection
+
+
+@push('scripts')
+
+@endpush
--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -1,61 +1,80 @@
 
 @extends('layouts.admin')
 
-@section('contenido')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <label for="tarea">
-                Tareas
-            </label>
-            <select 
-                name="tarea"
-                class="tarea_id form-control"
-                id="tarea"
-                data-placeholder="Choose one thing"
-                required
-            >
-                <option 
-                    value="0" 
-                    disabled="true" 
-                    selected="true"
-                    title="-Seleccione una tarea-"
-                >
-                    -Seleccione una tarea-
-                </option>
-                @foreach ($tareas as $tarea)
-                    <option 
-                        value="{{$tarea->id }}">
-                            {{$tarea->codigo . " " . $tarea->nombre}}
-                    </option>
-                @endforeach
-            </select>
-        </div>
+@section('titulo')
+    <div class="col-sm-6">
+        <h1 class="m-0">Tareas</h1>
     </div>
+@endsection
+
+@section('navegacion')
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="home">Home</a></li>
+            <li class="breadcrumb-item"><a href="tareas">Tareas</a></li>
+
+        </ol>
+    </div>
+@endsection
+
+@section('contenido')
 
 
+<div class="col-md-12">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Tareas</h3>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body">
 
-    <table id="tablaDetalle" class="table table-bordered table-condensed table-hover">
-        <thead>
-            <tr>
-                <th>Item</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($tareas as $tarea)
-                <tr> 
-                    <td>{{$tarea->codigo . " - " . $tarea->nombre}}</td>
+
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <select 
+                        name="tarea"
+                        class="tarea_id form-control"
+                        id="tarea"
+                        data-placeholder="Choose one thing"
+                        required
+                    >
+                        <option 
+                            value="0" 
+                            disabled="true" 
+                            selected="true"
+                            title="-Seleccione una tarea-"
+                        >
+                            -Seleccione una tarea-
+                        </option>
+                        @foreach ($tareas as $tarea)
+                            <option 
+                                value="{{$tarea->id }}">
+                                    {{$tarea->codigo . " " . $tarea->nombre}}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+<br>
+
+        <table id="tablaDetalle" class="table table-bordered table-condensed table-hover">
+            <thead>
+                <tr>
+                    <th>Item</th>
                 </tr>
-            @endforeach
-        </tbody>        
-    </table>
-
-
-
-
-
-
+            </thead>
+            <tbody>
+                @foreach ($tareas as $tarea)
+                    <tr> 
+                        <td>{{$tarea->codigo . " - " . $tarea->nombre}}</td>
+                    </tr>
+                @endforeach
+            </tbody>    
+        </table>
+      </div>
+    </div>
 </div>
+
 
 
 @endsection
